3bab889a6956b8ea40b9ffa7f24b463c2f6df8f2
XsquadKc <44047752+XsquadKc@users.noreply.github.com>
2022-08-31 14:27:27 +1000

    Add gamemanager object

--- The Game of Uni/Assets/Cards.cs
+++ The Game of Uni/Assets/Cards.cs
@@ -3,37 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Cards : MonoBehaviour
 {
-    [SerializeField]
-    ChanceCard[] chanceDeck;
-    [SerializeField]
-    FacultyCard[] degreeDeck;
 
 }
-public class ChanceCard
+[System.Serializable]
+public struct ChanceCard
 {
-    string name;
-    GameObject prefab;
-    bool choice;
-    bool holdable;
-    Vector3 modifiers;
-    int specialFunction;
-
-    ChanceCard(string Name, GameObject Prefab, bool Choice, bool Holdable, Vector3 Modifiers)
-    {
-        this.name = Name;
-        this.prefab = Prefab;
-        this.choice = Choice;
-        this.holdable = Holdable;
-        this.modifiers = Modifiers;
-    }
-    ChanceCard(string Name, GameObject Prefab, bool Choice, bool Holdable, int SpecialFunction)
-    {
-        this.name = Name;
-        this.prefab = Prefab;
-        this.choice = Choice;
-        this.holdable = Holdable;
-        this.specialFunction = SpecialFunction;
-    }
+    public string name;
+    public GameObject prefab;
+    public bool choice;
+    public bool holdable;
+    public Modifier modifiers;
+    public int specialFunction;
 
     public void AddToHand()
     {
@@ -48,15 +28,17 @@ public class ChanceCard
         }
     }
 }
-
-public class FacultyCard
+[System.Serializable]
+public struct FacultyCard
 {
-    string name;
-    Vector3 modifiers;
+    public string name;
+    public Vector3 modifiers;
 
-    FacultyCard(string Name, Vector3 Modifiers)
-    {
-        this.name = Name;
-        this.modifiers = Modifiers;
-    }
+}
+[System.Serializable]
+public struct Modifier
+{
+    public float S;
+    public float E;
+    public float K;
 }
--- The Game of Uni/Assets/GameManager.cs
+++ The Game of Uni/Assets/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField]
+    ChanceCard[] chanceDeck;
+    [SerializeField]
+    FacultyCard[] degreeDeck;
+
     void Start()
     {
         
