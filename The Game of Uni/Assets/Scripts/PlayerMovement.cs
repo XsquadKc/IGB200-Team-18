b5a3e1d2b53ac4baac7494f34fd851bddd588b7e
FlynnGrayling <fgrayling@outlook.com>
2022-09-04 14:57:33 +1000

    minigame

--- The Game of Uni/Assets/Scripts/PlayerMovement.cs
+++ The Game of Uni/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,18 +9,71 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform target;
     public float speed;
-    
+    private Vector3 previousPosition;
 
+    public float x;
+    public float y;
+    public float z;
+    public int savedValue;
 
     // Start is called before the first frame update
     void Start()
     {
-         
-
-
+        if (PlayerPrefs.HasKey("x") && PlayerPrefs.HasKey("y") && PlayerPrefs.HasKey("z") && PlayerPrefs.HasKey("sv"))
+        {
+            x = PlayerPrefs.GetFloat("x");
+            y = PlayerPrefs.GetFloat("y");
+            z = PlayerPrefs.GetFloat("z");
+            script.value = PlayerPrefs.GetInt("sv");
+            Vector3 posVec = new Vector3(x, y, z);
+            transform.position = posVec;
+        }
     }
 
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Minigame")
+        {
+            x = transform.position.x;
+            PlayerPrefs.SetFloat("x", x);
+            y = transform.position.y;
+            PlayerPrefs.SetFloat("y", y);
+            z = transform.position.z;
+            PlayerPrefs.SetFloat("z", z);
+            savedValue = script.value;
+            PlayerPrefs.SetInt("sv", savedValue);
 
+            previousPosition = transform.position;
+
+            SceneManager.LoadScene(sceneName: "Exam Minigame");
+            Debug.Log("trigger event");
+        }
+
+        
+    }
+
+    public void runMinigame()
+    {
+        x = transform.position.x;
+        PlayerPrefs.SetFloat("x", x);
+        y = transform.position.y;
+        PlayerPrefs.SetFloat("y", y);
+        z = transform.position.z;
+        PlayerPrefs.SetFloat("z", z);
+        savedValue = script.value;
+        PlayerPrefs.SetInt("sv", savedValue);
+
+        previousPosition = transform.position;
+
+        SceneManager.LoadScene(sceneName: "Exam Minigame");
+       // Debug.Log("trigger event");
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(sceneName: "SampleScene");
+    }
 
 
     void FixedUpdate()
@@ -28,9 +82,10 @@ public class PlayerMovement : MonoBehaviour
         
 
         Vector3 a = transform.position;
-        //Vector3 b = target.position;
         Vector3 c = GameObject.Find("Tile" + script.value.ToString()).transform.position;
         transform.position = Vector3.MoveTowards(a, c, speed);
+
+        
 
 
     }
