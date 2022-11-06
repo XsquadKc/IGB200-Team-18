bac63b7605c85d5ba2f7a7b131f08bb9a151b2c9
FlynnGrayling <fgrayling@outlook.com>
2022-11-06 11:41:38 +1000

    changed end scene

--- The Game of Uni/Assets/endscenemanager.cs
+++ The Game of Uni/Assets/endscenemanager.cs
@@ -5,10 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class endscenemanager : MonoBehaviour
 {
+    public GameObject ScreenE;
+    public GameObject ScreenS;
+    public GameObject ScreenK;
+
+    public float experience;
+    public float social;
+    public float knowledge;
+
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
+
+        ScreenE.SetActive(false);
+        ScreenS.SetActive(false);
+        ScreenK.SetActive(false);
+
+
+        if(gameManager.experienceScore > gameManager.socialScore && gameManager.experienceScore > gameManager.knowledgeScore)
+        {
+            ScreenE.SetActive(true);
+            Debug.Log("experience won");
+        }
+        if (gameManager.socialScore > gameManager.experienceScore && gameManager.socialScore > gameManager.knowledgeScore)
+        {
+            ScreenS.SetActive(true);
+            Debug.Log("social won");
+        }
+        if (gameManager.knowledgeScore > gameManager.socialScore && gameManager.knowledgeScore > gameManager.experienceScore)
+        {
+            ScreenK.SetActive(true);
+            Debug.Log("knowledge won");
+        }
+        else
+        {
+            ScreenE.SetActive(true);
+            Debug.Log("nothing won??");
+        }
     }
 
     // Update is called once per frame
